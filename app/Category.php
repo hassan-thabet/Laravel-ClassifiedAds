7dfb0be0ba81d5232fc101c691840e5b19006906
Hassan-thabet <dev.7assan@gmail.com>
2020-08-29 03:01:36 +0200

    My last commit

--- app/Category.php
+++ app/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = [
-        'category_name' , 'icon_url'
+       'id'  ,  'category_name' , 'icon_url'
     ];
 
     public function getDate()
--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -15,7 +15,7 @@ class CategoryResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'category_id' => $this->category_id,
+            'category_id' => $this->id,
             'category_name' => $this->category_name,
 
         ];
